167de1a39ae8541388cf6141b0439ad0f0bd05a6
matidza <patrickmatidza46@gmail.com>
2023-11-29 08:35:22 +0200

    Python Control Statement

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    return (a ^ b)
-pow(2,2)
+    return a ** b
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
-        elif num % 5 == 0:
-            print("Buzz ", end="")
-        elif num % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz ", end="")
+        elif i % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print(f"{num} ", end="")
-
-if __name__ == "__main__":
-    fizzbuzz()
+            print("{:d} ".format(i), end="")
