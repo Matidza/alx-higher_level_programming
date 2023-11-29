b8f5aca29e3dec7419ce5ec20957e6f1e93e4f87
matidza <patrickmatidza46@gmail.com>
2023-11-29 07:27:52 +0200

    Python Control Statement

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("{0:d} is positive".format(number))
+    print(f"{number} is positive")
 elif number == 0:
-    print("{0:d} is zero".format(number))
+    print(f"{number} is zero")
 else:
-    print("{0:d} is negative".format(number))
-    
+    print(f"{number} is negative")
+  
