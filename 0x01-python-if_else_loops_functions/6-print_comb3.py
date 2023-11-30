c7b27dab50f8c8407484dd5330b31fa7d52cdea6
matidza <patrickmatidza46@gmail.com>
2023-11-30 18:21:16 +0200

    Import libraries

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,3 +5,19 @@ while number <= 89:
         number += 1 + number // 10
     print("{:02d}".format(number), end='\n' if number == 89 else ", ")
     number += 1
+
+
+
+import random
+
+number = 1
+picked_number = random.randint(1, 50)
+
+while number <= 50:
+    if picked_number <= 25:
+        print(f"{picked_number} is less than or equal to 25.")
+        break
+    else:
+        print(f"{picked_number} is greater than 25.")
+        break
+    number += 1
