4c9489162702b75646c1ad6c5d4f932ef386a7a3
Matidza M.Z <125007667+Matidza@users.noreply.github.com>
2023-12-19 18:22:14 +0200

    Classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,4 +57,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-            
